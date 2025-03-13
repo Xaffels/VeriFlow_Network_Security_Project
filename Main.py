46fd5e4f0aed9965034861ccd707e956c9d46fef
Xaffels <jpharris68@gmail.com>
2025-03-12 19:52:58 -0500

    Finished socket communications v1

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 
 from VeriFlow.Network import Network
 import socket
+import sys
 
 ROUTE_VIEW = 1;
 BIT_BUCKET = 2;
@@ -16,39 +17,42 @@ def main():
 	print("Enter Port for controller")
 	port = input("> ")
 
-	socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-	socket.bind(ip, port)
-
-	while False:
-		socket.listen()
-		socket.accept()
-		pass
+	socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	socket.connect(ip, port)
 
 	generatedECs = network.getECsFromTrie();
 	network.checkWellformedness();
 	network.log(generatedECs);
+
 	while True:
 		print(" ");
 		print("Add rule by entering A#switchIP-rulePrefix-nextHopIP (eg.A#127.0.0.1-128.0.0.0/2-127.0.0.2)");
 		print("Remove rule by entering R#switchIP-rulePrefix-nextHopIP (eg.R#127.0.0.1-128.0.0.0/2-127.0.0.2)");
 		print("To exit type exit");
 
-		affectedEcs = set()
-		inputline = input('> ')
-		if (inputline.startswith("A")):
-			affectedEcs = network.addRuleFromString(inputline[2:]);
-			network.checkWellformedness(affectedEcs);
-		elif (inputline.startswith("R")):
-			affectedEcs = network.deleteRuleFromString(inputline[2:]);
-			network.checkWellformedness(affectedEcs);
-		elif ("exit" in inputline):
-			break;
-		else:
-			print("Wrong input format!");
-			continue;
+		inputline = socket.recv().decode()
+		print("Recieved: ", inputline)
+		if(inputline is not None):
+			affectedEcs = set()
+			#inputline = input('> ')
+			if (inputline.startswith("A")):
+				affectedEcs = network.addRuleFromString(inputline[2:]);
+				network.checkWellformedness(affectedEcs);
+			elif (inputline.startswith("R")):
+				affectedEcs = network.deleteRuleFromString(inputline[2:]);
+				network.checkWellformedness(affectedEcs);
+			elif ("exit" in inputline):
+				break;
+			else:
+				print("Wrong input format!");
+				continue;
 
-		print("");
-		network.log(affectedEcs);
+			print("");
+			network.log(affectedEcs);
+			inputline = None
+		else:
+			print("waiting for data...")
+			print(" ")
 
 if __name__ == '__main__':
 	main()
